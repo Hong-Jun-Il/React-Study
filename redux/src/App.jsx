9e3f284b66d3e4cfb02803a1d5652df100a196b6
Hong-Jun-Il <86016726+Hong-Jun-Il@users.noreply.github.com>
2024-07-09 13:28:27 +0900

    save

---
+++ redux/src/App.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+const RootWrapper = styled.div`
+
+`;
+
+export default App;
