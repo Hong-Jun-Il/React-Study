2f40e9d5e295aa17feca51b0ae0ae2e7bb95f07b
Hong-Jun-Il <86016726+Hong-Jun-Il@users.noreply.github.com>
2024-08-27 00:27:35 +0900

    내일 진짜 집중해서 정신 똑바로 차리고 해라

--- react-hook-form/src/components/RHFAutocomplete.tsx
+++ react-hook-form/src/components/RHFAutocomplete.tsx
@@ -1,15 +1,17 @@
-import { Autocomplete } from "@mui/material";
+import { Autocomplete, TextField } from "@mui/material";
 import { Controller, FieldValues, Path, useFormContext } from "react-hook-form";
 import { OptionType } from "../users/types/option";
 
 type PropsType<T extends FieldValues> = {
   name: Path<T>;
   options: OptionType[];
+  label: string;
 };
 
 export function RHFAutocomplete<T extends FieldValues>({
   name,
   options,
+  label,
 }: PropsType<T>) {
   const { control } = useFormContext();
 
@@ -17,7 +19,33 @@ export function RHFAutocomplete<T extends FieldValues>({
     <Controller
       control={control}
       name={name}
-      render={(params) => <Autocomplete />}
+      render={({ field: { value, onChange, ref }, fieldState: { error } }) => (
+        <Autocomplete
+          options={options}
+          value={value.map((id: string) =>
+            options.find((item) => item.id === id),
+          )}
+          getOptionLabel={(option) =>
+            options.find((item) => item.id === option.id)?.label ?? ""
+          }
+          isOptionEqualToValue={(option, newValue) => option.id === newValue.id}
+          onChange={(_, newValue) => {
+            onChange(newValue.map((item) => item.id));
+          }}
+          disableCloseOnSelect
+          multiple
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              fullWidth
+              inputRef={ref}
+              error={!!error}
+              helperText={error?.message}
+              label={label}
+            />
+          )}
+        />
+      )}
     />
   );
 }
