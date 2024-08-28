4ca44fc9d832d20355c2411b6f6e055cd64861de
Hong-Jun-Il <86016726+Hong-Jun-Il@users.noreply.github.com>
2024-08-28 23:58:23 +0900

    onChange 전

--- react-hook-form/src/components/Test/LoginTest.tsx
+++ react-hook-form/src/components/Test/LoginTest.tsx
@@ -1,9 +1,14 @@
-import { useFormContext } from "react-hook-form";
+import { Controller, useFormContext } from "react-hook-form";
 import { SchemaType } from "./schema";
 import { TextField } from "@mui/material";
+import { TestAutocomplete } from "./TestAutocomplete";
 
 export function LoginTest() {
-  const { register, handleSubmit } = useFormContext<SchemaType>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useFormContext<SchemaType>();
 
   const onSubmit = (data: SchemaType) => {
     console.log(data);
@@ -11,8 +16,28 @@ export function LoginTest() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <TextField {...register("id")} label="아이디" />
-      <TextField {...register("pw")} label="비밀번호" />
+      <TextField
+        {...register("id")}
+        label="아이디"
+        error={!!errors.id}
+        helperText={errors.id?.message}
+      />
+      <TextField
+        {...register("pw")}
+        label="비밀번호"
+        error={!!errors.pw}
+        helperText={errors.pw?.message}
+      />
+      <TestAutocomplete<SchemaType>
+        name="test"
+        label="Test"
+        options={[
+          { id: "1", test: "test1" },
+          { id: "2", test: "test2" },
+          { id: "3", test: "test3" },
+          { id: "4", test: "test4" },
+        ]}
+      />
       <input type="submit" />
     </form>
   );
--- react-hook-form/src/components/Test/schema.ts
+++ react-hook-form/src/components/Test/schema.ts
@@ -9,6 +9,7 @@ export const schema = z.object({
     .string()
     .min(1, { message: "비밀번호를 입력해주세요" })
     .max(20, { message: "20자 미만으로 입력해주세요" }),
+  test: z.array(z.string()).min(1).max(3),
 });
 
 export type SchemaType = z.infer<typeof schema>;
@@ -16,4 +17,5 @@ export type SchemaType = z.infer<typeof schema>;
 export const defaultValues: SchemaType = {
   id: "",
   pw: "",
+  test: [],
 };
