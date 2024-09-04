8196b5d9bd3b2118ec7fde4275399a5cbb1f3a5b
Hong-Jun-Il <86016726+Hong-Jun-Il@users.noreply.github.com>
2024-09-04 23:38:59 +0900

    test 파일 tailwind 세팅

--- ts-test/src/App.tsx
+++ ts-test/src/App.tsx
@@ -1,13 +1,9 @@
-
 function App() {
-  
-
   return (
-    <>
+    <main className="flex justify-center items-center min-h-screen">
       
-    </>
-  )
+    </main>
+  );
 }
 
-
-export default App
+export default App;
--- ts-test/src/main.tsx
+++ ts-test/src/main.tsx
@@ -1,22 +1,21 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import { GlobalStyle } from './styles/GlobalStyle.ts'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createRoot } from "react-dom/client";
+import "../index.css";
+import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: 3,
-      retryDelay: 1000
-    }
-  }
-})
+      retryDelay: 1000,
+    },
+  },
+});
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   // <StrictMode>
   <QueryClientProvider client={queryClient}>
-    <GlobalStyle />
     <App />
   </QueryClientProvider>
   // </StrictMode>,
-)
+);
