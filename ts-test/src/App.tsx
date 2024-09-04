f4db19e2bbf62ffe63fda6f92c5f1a58e84354ed
Hong-Jun-Il <86016726+Hong-Jun-Il@users.noreply.github.com>
2024-09-05 00:37:31 +0900

    충돌 방지용 save

--- ts-test/src/App.tsx
+++ ts-test/src/App.tsx
@@ -1,8 +1,8 @@
+
+
 function App() {
   return (
-    <main className="flex justify-center items-center min-h-screen">
-      
-    </main>
+    <main className="flex justify-center items-center min-h-screen bg-sky-100"></main>
   );
 }
 
